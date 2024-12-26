d0577f8dc010c5c502a0468689cbccf7579a110a
rgbalex <alex.m.angelidis@gmail.com>
2024-12-26 11:56:08 +0000

    changing to work in 2d

--- 2d_projection.py
+++ 2d_projection.py
@@ -1,5 +1,6 @@
 import glfw
 from OpenGL.GL import *
+from glfw import swap_buffers, poll_events
 
 
 def key_event(window, key, scancode, action, mods):
@@ -7,10 +8,40 @@ def key_event(window, key, scancode, action, mods):
         glfw.set_window_should_close(window, True)
 
 
-def draw():
+def change_size_event(window, width, height):
+    pass
+
+
+def render():
     glClear(GL_COLOR_BUFFER_BIT)
-    glBegin(GL_TRIANGLES)
-    glVertex2f(-0.5, -0.5)
-    glVertex2f(0.5, -0.5)
-    glVertex2f(0.0, 0.5)
+    glPointSize(10)
+    glColor3f(1, 0, 0)
+    glBegin(GL_POINTS)
+    glVertex2f(0, 0)
+    glVertex2f(0.25, 0.25)
+    glVertex2f(0.5, 0.5)
     glEnd()
+
+
+def main():
+    while not glfw.window_should_close(window):
+        render()
+        swap_buffers(window)
+        poll_events()
+    return 0
+
+
+if __name__ == "__main__":
+    glfw.init()
+    glfw.window_hint(glfw.RESIZABLE, glfw.TRUE)
+
+    window = glfw.create_window(640, 480, "Rope Simulation", None, None)
+
+    glfw.make_context_current(window)
+
+    glfw.set_key_callback(window, key_event)
+    glfw.set_window_size_callback(window, change_size_event)
+
+    change_size_event(window, 640, 480)
+
+    main()
